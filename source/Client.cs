bdbfb5faabb65a85ae6f9f731587c9afbe36ea71
Aminadaven <61430170+Aminadaven@users.noreply.github.com>
2020-03-25 22:34:51 +0200

    Initial commit

---
+++ source/Client.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace RemoteCMD
+{
+    class Client
+    {
+        private const int port = 1024, bufferSize = 4096;
+        private static string location = "DISCONNECTED", hostName = "";
+        private const string TITLE = "Remote CMD Client by Aminadav";
+        private static TcpClient client = new TcpClient();
+        private static NetworkStream ns;
+        static void Main()
+        {
+            Console.Title = TITLE;
+            Boolean active = true;
+            while (active)
+            {
+                Console.Write(Location() + ">");
+                string line = Console.ReadLine();
+
+                string[] brokenLine = line.Split(" ");
+                switch (brokenLine[0].ToLower())
+                {
+                    case "connect":
+                        Connect(brokenLine[1]);
+                        break;
+                    case "shutdown":
+                        Console.WriteLine(Command(line));
+                        Disconnect();
+                        break;
+                    case "disconnect":
+                        Disconnect();
+                        break;
+                    case "cd":
+                        Cd(brokenLine);
+                        break;
+                    case "drive":
+                        Drive(brokenLine[1]);
+                        break;
+                    case "exit":
+                        Disconnect();
+                        active = false;
+                        break;
+                    default:
+                        Console.WriteLine(Command(line));
+                        break;
+                }
+            }
+        }
+
+        private static void Connect(string host)
+        {
+            try
+            {
+                client = new TcpClient(host, port);
+                ns = client.GetStream();
+                //sets the location
+                byte[] bytes = new byte[bufferSize];
+                int bytesRead = ns.Read(bytes, 0, bytes.Length);
+                location = Encoding.ASCII.GetString(bytes, 0, bytesRead);
+                hostName = host + ":" + port + "\\\\";
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Title = TITLE + " - Connected to " + host + " at port: " + port;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static string Command(string command)
+        {
+            if (!client.Connected)
+            {
+                Console.Error.WriteLine("Usage: Connect <hostName>");
+                return null;
+            }
+            string result = "";
+            try
+            {
+                ns.Write(Encoding.ASCII.GetBytes(command));
+                //if (ns.DataAvailable)
+                //{
+                byte[] bytes = new byte[bufferSize];
+                int bytesRead = ns.Read(bytes, 0, bytes.Length);
+                result = Encoding.ASCII.GetString(bytes, 0, bytesRead);
+                //}
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return result;
+        }
+        static void Disconnect()
+        {
+            try
+            {
+                ns.Close();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            hostName = "";
+            location = "DISCONNECTED";
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Title = TITLE;
+        }
+
+        private static string Location()
+        {
+            return hostName + location;
+        }
+
+        private static void Drive(string newDrive)
+        {
+            string result = Command("drive " + (newDrive.EndsWith("\\") ? newDrive : $"{newDrive}\\"));
+            if (result.Equals("true"))
+            {
+                location = newDrive;
+            }
+            else
+            {
+                Console.Error.WriteLine("Server could not find {0}", newDrive);
+            }
+        }
+
+        private static void Cd(string[] brokenLine)
+        {
+            if (brokenLine.Length <= 1)
+            {
+                Console.WriteLine(Location());
+                return;
+            }
+            else if (brokenLine[1].StartsWith(".."))
+            {
+                string dir = Directory.GetParent(location).FullName;
+                if (brokenLine[1].Length > 2)
+                {
+                    string newDir = brokenLine[1].Substring(2);
+                    location = Path.Combine(dir, newDir);
+                }
+                else
+                {
+                    location = dir;
+                }
+                Command("setPath " + location);
+            }
+            else
+            {
+                var result = Command("cd " + brokenLine[1]);
+                if (result.Equals("exist"))
+                {
+                    location += brokenLine[1];
+                }
+                else
+                {
+                    Console.Error.WriteLine("Server could not find {0}", brokenLine[1]);
+                }
+            }
+        }
+    }
+}
---
+++ source/Server.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace RemoteCMD
+{
+    public class Server
+    {
+        private static NetworkStream ns;
+        private static bool connected;
+        private const int port = 1024, bufferSize = 4096;
+        private static string drive = "", path = "";
+        private const string TITLE = "Remote CMD Server by Aminadav";
+        private const string commands = "shutdown\ncd <dir>\ndrive <drivename>:\ndir\nmkdir <dir>\n" +
+            "rndir <dir>\ncopydir <dir> <destination>\nmovedir <dir> <destination>\nmake <file>\n" +
+            "del <file>\nmove <file> <destination>\ncopy <file <destination>\nrename <file> <newname>\n" +
+            "read <file>\nwrite <file> - enter \"<EOF>\" to end writing.";
+        static void Main()
+        {
+            bool on = true;
+            var listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.Title = TITLE + " at: " + listener.LocalEndpoint.ToString();
+            while (on)
+            {
+                Console.Write("Waiting for connection... ");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Connection accepted.");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Title = TITLE + " - Connected to: " + client.Client.RemoteEndPoint.ToString() + ", my endPoint: " + client.Client.LocalEndPoint.ToString();
+                ns = client.GetStream();
+                SetPath();
+                SendResult(Path());
+                connected = true;
+                while (connected)
+                {
+                    string[] bm = GetCommand().Split(" ");
+                    if (!connected) break;
+                    switch (bm[0])
+                    {
+                        case "shutdown":
+                            connected = false;
+                            on = false;
+                            SendResult("Server is turning off.");
+                            break;
+                        case "cd":
+                            Cd(bm[1]);
+                            break;
+                        case "dir":
+                            Dir();
+                            break;
+                        case "drive":
+                            Drive(bm[1]);
+                            break;
+                        case "setPath":
+                            path = bm[1].Substring(3);
+                            break;
+                        case "mkdir":
+                            MkDir(bm[1]);
+                            break;
+                        case "rmdir":
+                            RmDir(bm[1]);
+                            break;
+                        case "movedir":
+                            MoveDir(bm[1], bm[2]);
+                            break;
+                        case "copydir":
+                            CopyDir(bm[1], bm[2]);
+                            break;
+                        case "move":
+                            Move(bm[1], bm[2]);
+                            break;
+                        case "copy":
+                            Copy(bm[1], bm[2]);
+                            break;
+                        case "del":
+                            Del(bm[1]);
+                            break;
+                        case "make":
+                            Make(bm[1]);
+                            break;
+                        case "rename":
+                            Rename(bm[1], bm[2]);
+                            break;
+                        case "read":
+                            Read(bm[1]);
+                            break;
+                        //case "write":
+                        //    Write(bm[1]);
+                        //    break;
+                        default:
+                            Help();
+                            break;
+                    }
+                }
+                try
+                {
+                    ns.Close();
+                    client.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            listener.Stop();
+        }
+
+        private static void Help()
+        {
+            SendResult(commands);
+        }
+
+        private static void Read(string file)
+        {
+            string data = "The file: " + file + " not found, or too large (>" + bufferSize + " bytes) to read properly.";
+            if (File.Exists(Path() + "\\" + file) && new FileInfo(Path() + "\\" + file).Length > bufferSize)
+            {
+                data = File.ReadAllText(Path() + "\\" + file);
+            }
+            SendResult(data);
+        }
+
+        private static void Write(string fileName)
+        {
+            string data = "The file: " + fileName + " not found, or is read-only.";
+            FileInfo file = new FileInfo(Path() + "\\" + fileName);
+            if (file.Exists && !file.IsReadOnly)
+            {
+                var stream = file.AppendText();
+                string line = GetCommand();
+                while (!line.EndsWith("<EOF>"))
+                {
+                    stream.WriteLine(line);
+                    line = GetCommand();
+                }
+                line = line.Replace("<EOF>", "");
+                stream.Write(line);
+                stream.Flush();
+                stream.Close();
+                stream.Dispose();
+                data = $"The file {fileName} has been writed.";
+            }
+            SendResult(data);
+        }
+
+        private static void Rename(string oldName, string newName)
+        {
+            if (!File.Exists(Path() + "\\" + oldName) || File.Exists(Path() + "\\" + newName))
+            {
+                File.Move(Path() + "\\" + newName, Path() + "\\" + newName);
+            }
+            Success();
+        }
+
+        private static void Make(string newFile)
+        {
+            if (!File.Exists(Path() + "\\" + newFile))
+            {
+                bool s = true;
+                try
+                {
+                    File.Create(Path() + "\\" + newFile).Dispose();
+                }
+                catch (Exception e)
+                {
+                    SendResult(e.Message);
+                    s = false;
+                }
+                if (s)
+                    Success();
+            }
+        }
+
+        private static void Drive(string newDrive)
+        {
+            DriveInfo di = new DriveInfo(newDrive);
+            if (di.IsReady)
+            {
+                drive = newDrive;
+                path = "";
+                SendResult("true");
+                return;
+            }
+            SendResult("false");
+        }
+
+        private static void MkDir(string newDir)
+        {
+            if (!Directory.Exists(Path() + "\\" + newDir))
+            {
+                Directory.CreateDirectory(Path() + "\\" + newDir);
+            }
+            Success();
+        }
+
+        private static void RmDir(string dir)
+        {
+            if (Directory.Exists(Path() + "\\" + dir))
+            {
+                Directory.Delete(Path() + "\\" + dir);
+            }
+            Success();
+        }
+
+        private static void Del(string file)
+        {
+            if (!File.Exists(Path() + "\\" + file))
+            {
+                Fail();
+                return;
+            }
+            File.Delete(Path() + "\\" + file);
+            Success();
+        }
+
+        private static void Move(string file, string dest)
+        {
+            if (!File.Exists(Path() + "\\" + file) || Directory.Exists(dest + "\\" + file))
+            {
+                Fail();
+                return;
+            }
+            File.Move(Path() + "\\" + file, dest + "\\" + file);
+            Success();
+        }
+
+        private static void Copy(string file, string dest)
+        {
+            if (!File.Exists(Path() + "\\" + file) || Directory.Exists(dest + "\\" + file))
+            {
+                Fail();
+                return;
+            }
+            File.Copy(Path() + "\\" + file, dest + "\\" + file);
+            Success();
+        }
+
+        private static void MoveDir(string dir, string dest)
+        {
+            if (!Directory.Exists(Path() + "\\" + dir) || Directory.Exists(dest + "\\" + dir))
+            {
+                Fail();
+                return;
+            }
+            Directory.Move(Path() + "\\" + dir, dest + "\\" + dir);
+            Success();
+        }
+
+        private static void CopyDir(string dir, string dest)
+        {
+            if (!Directory.Exists(Path() + "\\" + dir) || Directory.Exists(dest + "\\" + dir))
+            {
+                Fail();
+                return;
+            }
+            CPD(Path() + "\\" + dir, dest + "\\" + dir);
+            File.Copy(Path() + "\\" + dir, dest + "\\" + dir);
+            Success();
+        }
+
+        private static void CPD(string dir, string dest)
+        {
+            Directory.CreateDirectory(dest);
+            foreach (string file in Directory.GetFiles(dir))
+            {
+                File.Copy(dir + "\\" + file, dest + "\\" + file);
+            }
+            foreach (string subDir in Directory.GetDirectories(Path() + "\\" + dir))
+            {
+                CPD(dir + "\\" + subDir, dest + "\\" + subDir);
+            }
+        }
+
+        private static void Dir()
+        {
+            SendResult(SearchDirectoryTree(new DirectoryInfo(Path())));
+        }
+
+        static string SearchDirectoryTree(DirectoryInfo root)
+        {
+            FileInfo[] files;
+            DirectoryInfo[] subDirs;
+            string result = "";
+            // First, process all the files directly under this folder
+            try
+            {
+                files = root.GetFiles("*.*");
+                // Now find all the subdirectories under this directory.
+                subDirs = root.GetDirectories();
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+            if (subDirs != null)
+            {
+                result += ("Dirs: \n");
+                foreach (DirectoryInfo dir in subDirs)
+                {
+                    string dirDetails = dir.LastAccessTime.ToShortDateString() + "  " + dir.LastAccessTime.ToShortTimeString() + "\t" + dir.Name;
+                    result += (dirDetails + "\n");
+                }
+            }
+            if (files != null)
+            {
+                result += ("Files: " + "\n");
+                foreach (FileInfo fi in files)
+                {
+                    string fileDetails = fi.LastAccessTime.ToShortDateString() + "  " + fi.LastAccessTime.ToShortTimeString() + "\t" + fi.Name;
+                    result += (fileDetails + "\n");
+                }
+            }
+            return result;
+        }
+
+        private static void Cd(string newDir)
+        {
+
+            string newPath = path + "\\" + newDir;
+            if (!Directory.Exists(newPath))
+            {
+                SendResult("Not Exist");
+                return;
+            }
+            path = newPath;
+            SendResult("exist");
+        }
+
+        static string Path()
+        {
+            return drive + path;
+        }
+
+        static void SetPath()
+        {
+            string[] drives = Environment.GetLogicalDrives();
+            foreach (string dr in drives)
+            {
+                DriveInfo di = new DriveInfo(dr);
+                if (di.IsReady)
+                {
+                    drive = dr;
+                    break;
+                }
+            }
+        }
+
+        private static void SendResult(string result)
+        {
+            byte[] byteTime = Encoding.ASCII.GetBytes(result);
+            ns.Write(byteTime, 0, byteTime.Length);
+        }
+
+        private static string GetCommand()
+        {
+            try
+            {
+                byte[] bytes = new byte[bufferSize];
+                int bytesRead = ns.Read(bytes, 0, bytes.Length);
+                return Encoding.ASCII.GetString(bytes, 0, bytesRead);
+            }
+            catch (IOException e)
+            {
+                Console.Beep();
+                Console.Error.WriteLine(e.Message);
+                connected = false;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Title = TITLE;
+
+                return "";
+            }
+        }
+
+        private static void Fail()
+        {
+            SendResult("Command Failed.");
+        }
+
+        private static void Success()
+        {
+            SendResult("Command Success.");
+        }
+    }
+}
